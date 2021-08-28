2d98864f6cc217a91ca26d54ef031cbaca3724f5
Thomas Emerson <emersonvisuals@gmail.com>
2021-08-28 15:38:03 +1000

    discount attempt

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
+// Toggle 
 const toggleContainer = document.getElementById('toggle-container');
 const toggle = document.getElementById('toggle');
 const toggleActive = document.querySelector('#toggle-container.active');
+let amount = document.querySelector('.sliderValue span');
 
 toggleContainer.addEventListener('click',() => {
 
     toggleContainer.classList.toggle('active');
     toggle.classList.toggle('active');
-    
+    amount.classList.toggle ('active');
 });
 
+
+// Slider
 const slider = document.querySelector('input');
-const amount = document.querySelector('.sliderValue span');
+const pageViews = document.querySelector('.card-heading span');
 
 amount.textContent = '$' + slider.value + ".00";
+pageViews.textContent = slider.value*5 + "K";
 
 slider.oninput = function() {
+    
     amount.textContent = "$" + this.value + ".00";
+
+    pageViews.textContent = this.value*5 + 'k';
+
     var value = (this.value-this.min)/(this.max-this.min)*100
-    this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
+    this.style.background = 'linear-gradient(to right, hsl(174, 86%, 45%) 0%, hsl(174, 86%, 45%) ' + value + '%, hsl(224, 65%, 95%) ' + value + '%, hsl(224, 65%, 95%) 100%)'
 }
+
 
 
 
