b5aeecb950e5867bdd23d5e9b09cb4ddb469e086
Thomas Emerson <emersonvisuals@gmail.com>
2021-08-29 13:06:59 +1000

    updated files

--- main.js
+++ main.js
@@ -1,32 +1,18 @@
-// Toggle 
-const toggleContainer = document.getElementById('toggle-container');
-const toggle = document.getElementById('toggle');
-const toggleActive = document.querySelector('#toggle-container.active');
-let amount = document.querySelector('.sliderValue span');
-
-toggleContainer.addEventListener('click',() => {
-
-    toggleContainer.classList.toggle('active');
-    toggle.classList.toggle('active');
-    amount.classList.toggle ('active');
-});
+// Variables
+let toggleContainer = document.getElementById('toggle-container');
+let toggle = document.getElementById('toggle');
+let toggleActive = document.querySelector('#toggle-container.active');
+let duration = document.querySelector('.month');
+let price = document.querySelector(".priceValue");
+let range = document.querySelector('#myinput');
+let pageViews = document.querySelector(".page-views span");
 
 
-// Slider
-const slider = document.querySelector('input');
-const pageViews = document.querySelector('.card-heading span');
-
-amount.textContent = '$' + slider.value + ".00";
-pageViews.textContent = slider.value*5 + "K";
-
-slider.oninput = function() {
-    
-    amount.textContent = "$" + this.value + ".00";
-
-    pageViews.textContent = this.value*5 + 'k';
+// Slider Progress Bar
+range.oninput = function() {
 
     var value = (this.value-this.min)/(this.max-this.min)*100
-    this.style.background = 'linear-gradient(to right, hsl(174, 86%, 45%) 0%, hsl(174, 86%, 45%) ' + value + '%, hsl(224, 65%, 95%) ' + value + '%, hsl(224, 65%, 95%) 100%)'
+    this.style.background = 'linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%)' + value + '%, hsl(224, 65%, 95%) ' + value + '%, hsl(224, 65%, 95%) 100%)'
 }
 
 
